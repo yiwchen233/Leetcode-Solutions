830873498bdd0977ea4de923359fabdb8fb89ab9
yiwchen233 <102659677+yiwchen233@users.noreply.github.com>
2022-04-06 14:03:21 +0800

    Create 24.cpp

---
+++ Solutions/Recursion/24.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==nullptr) return nullptr;
+        if(head->next==nullptr) return head;
+        ListNode *nfnode = head->next;
+        head->next = swapPairs(nfnode->next);
+        nfnode->next = head;
+        return nfnode;
+    }
+};

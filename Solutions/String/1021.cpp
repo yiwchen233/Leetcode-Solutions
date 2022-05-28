8ba5ceb39e842ab3de8d4ac4aad6085186553955
yiwchen233 <102659677+yiwchen233@users.noreply.github.com>
2022-05-28 11:47:19 +0800

    Create 1021.cpp

---
+++ Solutions/String/1021.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        stack<char> st;
+        for(auto c:s){
+            if (c == ')') st.pop();
+            if (!st.empty()) res.push_back(c);
+            if (c == '(') st.emplace(c);
+        }
+        return res;
+    }
+};

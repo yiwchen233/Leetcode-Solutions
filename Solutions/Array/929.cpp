8a5cba71910f5e4f6cb56fca379675ea7261ce89
yiwchen233 <102659677+yiwchen233@users.noreply.github.com>
2022-06-04 18:42:25 +0800

    Create 929.cpp

---
+++ Solutions/Array/929.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string> &emails) {
+        unordered_set<string> emailSet;
+        for (auto &email: emails) {
+            string local;
+            for (char c: email) {
+                if (c == '+' || c == '@') {
+                    break;
+                }
+                if (c != '.') {
+                    local += c;
+                }
+            }
+            emailSet.emplace(local + email.substr(email.find('@')));
+        }
+        return emailSet.size();
+    }
+};
